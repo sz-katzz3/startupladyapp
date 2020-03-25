18a71eb06992c356fc7e04fc732a527728700fba
kat <katavamp@gmail.com>
2020-03-25 20:22:02 +0900

    working

---
+++ src/pages/about/about.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit, Inject } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Leader } from '../../shared/leader';
+import { LeaderProvider } from '../../providers/leader/leader';
+
+/**
+ * Generated class for the AboutPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-about',
+  templateUrl: 'about.html',
+})
+export class AboutPage implements OnInit {
+  leaders: Leader[];
+  errMess: string;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams,
+    private leaderService: LeaderProvider,
+    @Inject('BaseURL') private BaseURL) { }
+  
+  ngOnInit() {
+    this.leaderService.getLeaders()
+      .subscribe(leaders => this.leaders = leaders,
+        errmess => this.errMess = <any>errmess);
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad AboutPage');
+  }
+
+}
